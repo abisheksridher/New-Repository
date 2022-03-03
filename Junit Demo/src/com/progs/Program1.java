6babccf4e523e1e294e6361003e906cdfaae200a
abisheksridher <abisheksridher2012@gmail.com>
2022-03-03 10:14:33 +0530

    abi commit

--- Junit Demo/src/com/progs/Program1.java
+++ Junit Demo/src/com/progs/Program1.java
@@ -12,7 +12,7 @@ public class Program1 {
 		int sum=0;
 		System.out.println("Hello");
 		System.out.println("Hello Abishek");
-		
+		System.out.println("Hello Abi");
 		for(char c: input.toCharArray())
 		{
 			if(!(Character.isLetterOrDigit(c)))
